07b721c1822469f27f00422c9532676f8423c857
Vinni-Cedraz <vcedraz-@student.42sp.org.br>
2022-11-14 02:36:54 -0300

    Changes to be committed: command to changed default text editor for git to neovim is now aliased to "git_editor"

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/zsh
+
+# create .zshrc if it doesn't exist
+touch ~/.zshrc
+
+# write shell configurations to .zshrc
+echo '# outsourcing my shell configurations directly from dotfiles repo:' >> ~/.zshrc
+echo 'if [ -f ~/.dotfiles/zshrc ]; then' >> ~/.zshrc
+echo '	source ~/.dotfiles/zshrc' >> ~/.zshrc
+echo 'else' >> ~/.zshrc
+echo '	print "404: shell configurations not found."' >> ~/.zshrc
+echo 'fi' >> ~/.zshrc
+echo '# outsourcing end;' >> ~/.zshrc
