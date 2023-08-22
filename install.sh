de5fd9c673ea0f5f909c1b52d2c4d5203f8b94b3
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-08-22 13:53:40 -0300

    I hope this is working now but it is probably still messy... So hard to test...

--- install.sh
+++ install.sh
@@ -5,22 +5,22 @@ mkdir -p ~/.config/fish/
 touch ~/.config/fish/config.fish
 cp Xmodmap ~/.Xmodmap
 xmodmap ~/.Xmodmap
-cp -r fonts ~/.fonts
+cp -r fonts/* ~/.fonts
 fc-cache -f -v ~/.fonts
 
 mkdir -p ~/.config/wezterm
 cp wezterm.lua ~/.config/wezterm/wezterm.lua
 
 ## install fish plugin managers
-#curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 
-## fix nvm for fish:
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-fisher install FabioAntunes/fish-nvm edc/bass
 mkdir $NVM_DIR
+# install nvm on bash first
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-## use node 16
+
+## fix nvm for fish:
+fisher install FabioAntunes/fish-nvm edc/bass
 nvm install 16
 nvm use 16
 
